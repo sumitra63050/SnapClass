fab5c419c4799b868455de3e545d20a068392b1c
sumitra63050 <sumitrachoudhary63050@gmail.com>
2026-09-11 12:15:31 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+def footer_home():
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Made with ❤️ by 
+        <span style="font-size:1.2rem;color:black;">Sumitra</span> </p>  
+        </div>
+                
+                """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Made with ❤️ by 
+        <span style="font-size:1.2rem;color:black;">Sumitra</span> </p>  
+        </div>
+                
+                """, unsafe_allow_html=True)
