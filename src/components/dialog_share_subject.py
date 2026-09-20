24bc9e4ac22f8ce6b280a4ba44e08ad960b516ea
sumitra63050 <sumitrachoudhary63050@gmail.com>
2026-09-20 11:30:54 +0530

    change domain name 2

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-smart-attendance.streamlit.app"
+    app_domain = "snapclass-student-attendance.streamlit.app"
     join_url = f"https://{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
