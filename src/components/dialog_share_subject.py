aa041ac9847af7fca5306f692221cb4af47924a4
sumitra63050 <sumitrachoudhary63050@gmail.com>
2026-09-20 14:36:27 +0530

    correct join url

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -7,7 +7,7 @@ import io
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
     app_domain = "snapclass-digital-attendance.streamlit.app"
-    join_url = f"{app_domain}/?join-code={subject_code}"
+    join_url = f"https://{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
 
