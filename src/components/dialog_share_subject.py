520c3e60ad3ecc41ebc28e57c260c8e62eb3713b
sumitra63050 <sumitrachoudhary63050@gmail.com>
2026-09-20 12:15:04 +0530

    domain change to local

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,8 +6,8 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-digital-attendance.streamlit.app"
-    join_url = f"https://{app_domain}/?join-code={subject_code}"
+    app_domain = "http://localhost:8501"
+    join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
 
